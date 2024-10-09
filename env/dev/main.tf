5c49905cfe36d2963ea849df6aca834c462e958a
jxtsy <aj8984524@gmail.com>
2024-10-09 17:30:55 -0600

    Deploy-dev

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -14,7 +14,7 @@ module "dev_vm" {
   server_name                = "IN-Server-Jaramillo"
   location                   = "eastus2"
   mapbox_access_token        = var.MAPBOX_ACCESS_TOKEN
-  mongo_url                  = var.MONGO_URL
+  mongo_url                  = var.MONGO_URL 
   subnet_name                = "IN-SUBNET-Jaramillo"
   mongo_initdb_root_username = var.MONGO_INITDB_ROOT_USERNAME
   mongo_initdb_root_password = var.MONGO_INITDB_ROOT_PASSWORD
@@ -25,4 +25,4 @@ module "dev_vm" {
 }
 
  
- 
+  
